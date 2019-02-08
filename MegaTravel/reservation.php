f9f20044156fd7482ed2f82b5200b8db905ea52e
Jwakefield97 <jacobstevenwakefield@gmail.com>
2019-02-08 03:32:00 -0600

    add basic fields for form

--- MegaTravel/reservation.php
+++ MegaTravel/reservation.php
@@ -5,7 +5,23 @@
     <div class="card">
         <h2 class="card-header p-2 underline">Make a Reservation</h2>
         <div class="card-body">
-            
+            <form action="/MegaTravel/forms/reservation_form.php" method="post">
+                <div class="form-row">
+                    <div class="col-md-4 mb-3">
+                        <label for="firstname">First name</label>
+                        <input name="firstname" type="text" class="form-control" id="firstname" placeholder="First name" value="firstname" required>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="lastname">Last name</label>
+                        <input name="lastname" type="text" class="form-control" id="lastname" placeholder="Last name" value="lastname" required>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary float-right">Submit</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
     </div>

19a3dca5918f1619757627e5d38f08d555462fb2
Jwakefield97 <jacobstevenwakefield@gmail.com>
2019-02-08 02:42:01 -0600

    add about us page

--- MegaTravel/about.php
+++ MegaTravel/about.php
@@ -1,8 +1,34 @@
 <?php include 'header.php'; ?>
 
 <div class="container-fluid">
-    <div class="col-12">
-        this is the about page.
+    <div class="row">
+        <div class="col-2 mx-auto">
+            <h1>About Us</h1>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-12">
+            <div class="card">
+                <h2 class="card-header p-2 underline">Our History</h2>
+                <div class="card-body">
+                    <p class="text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed quam felis. Etiam tincidunt non lectus nec malesuada. Quisque ullamcorper vehicula sem, non pulvinar leo pharetra eget. Cras faucibus odio non neque porttitor feugiat. Curabitur dolor risus, sodales ut orci dapibus, gravida finibus urna. Nam vel semper felis. Nam nec tempus sapien. Integer tempor eu lectus ac pulvinar. Proin bibendum sapien tristique, auctor ex ut, suscipit sapien. Quisque vel nulla dui. Praesent pulvinar, risus nec tincidunt accumsan, tellus orci aliquam est, eget sagittis nisl neque nec est. Aenean volutpat, nisl ut suscipit pharetra, libero libero consequat erat, a dapibus purus lacus ac dui.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-12">
+            <div class="card">
+                <h2 class="card-header p-2 underline">Our Goals</h2>
+                <div class="card-body">
+                    <p class="text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed quam felis. Etiam tincidunt non lectus nec malesuada. Quisque ullamcorper vehicula sem, non pulvinar leo pharetra eget. Cras faucibus odio non neque porttitor feugiat. Curabitur dolor risus, sodales ut orci dapibus, gravida finibus urna. Nam vel semper felis. Nam nec tempus sapien. Integer tempor eu lectus ac pulvinar. Proin bibendum sapien tristique, auctor ex ut, suscipit sapien. Quisque vel nulla dui. Praesent pulvinar, risus nec tincidunt accumsan, tellus orci aliquam est, eget sagittis nisl neque nec est. Aenean volutpat, nisl ut suscipit pharetra, libero libero consequat erat, a dapibus purus lacus ac dui.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
